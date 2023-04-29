50e7f388a5836402aa14ac4a3227cb2fb9eb31ab
Zhenyu Yin <zhenyin@amazon.com>
2023-04-29 23:56:54 +0800

    modify logic

--- scripts/install_interruption_handler_event_bus.sh
+++ scripts/install_interruption_handler_event_bus.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 export KARPENTER_VERSION=v0.27.3
-export CLUSTER_NAME=$1
 export AWS_DEFAULT_REGION="us-west-2"
 export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 export TEMPOUT=$(mktemp)
 
 aws cloudformation deploy \
-    --stack-name "Karpenter-${CLUSTER_NAME}" \
-    --template-file ./cloudformations/Interruption_handler_event_bus.yaml \
-    --capabilities CAPABILITY_NAMED_IAM \
-    --parameter-overrides "ClusterName=${CLUSTER_NAME}"; \
+    --stack-name "Karpenter-interruption_handler_event_bus" \
+    --template-file ./cloudformations/interruption_handler_event_bus.yaml \
+    --capabilities CAPABILITY_NAMED_IAM 
 
