332bc5a23c107616700f1a44deb17c89485a0203
Zhenyu Yin <zhenyin@amazon.com>
2023-05-01 20:02:47 +0800

    add clean up logic

--- scripts/clean_all.sh
+++ scripts/clean_all.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-export CLUSTER_NAME=$1
-export CLUSTER_NAME_1=$2
-
 
 # Get a list of all contexts in the Kubernetes configuration file
 contexts=$(kubectl config get-contexts -o name) 
@@ -12,12 +9,28 @@ for context in $contexts; do
   echo "Listing pods for context: $context"
   kubectl config use-context $context
   helm uninstall karpenter --namespace karpenter
+  kubectl delete deployment inflate
 done && \
 
 
-aws cloudformation delete-stack --stack-name "Karpenter-${CLUSTER_NAME}-role-and-interruption-queue" && \
-aws cloudformation delete-stack --stack-name "Karpenter-Event-Bus" && \
-aws cloudformation delete-stack --stack-name "Karpenter-${CLUSTER_NAME}-FIS-experiments"  && \
+# Delete Event Bus
+aws cloudformation delete-stack --stack-name "Karpenter-Event-Bus" 
 
-eksctl delete cluster --name "${CLUSTER_NAME}" && \
-eksctl delete cluster --name "${CLUSTER_NAME_1}" && \
+
+
+# delete all cluster s
+cluster_set=$(eksctl get cluster)
+
+echo "$cluster_set" | while read -r line; do
+  # skip the header row
+  if [[ $line == NAME* ]]; then
+    continue
+  fi
+  CLUSTER_NAME=$(echo "$line" | awk '{print $1}')
+
+  # print cluster based configuration
+  aws cloudformation delete-stack --stack-name "Karpenter-${CLUSTER_NAME}-role-and-interruption-queue" 
+  aws cloudformation delete-stack --stack-name "Karpenter-${CLUSTER_NAME}-FIS-experiments" 
+
+  eksctl delete cluster --name ${CLUSTER_NAME}
+done
--- scripts/start_fis_experiments.sh
+++ scripts/start_fis_experiments.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
 
-export cluster1_tag='SpotInterruptionTest-test-eks'
-export cluster2_tag='SpotInterruptionTest-test-eks-1'
+$ export cluster1_tag='SpotInterruptionTest-test-eks'
+$ export cluster2_tag='SpotInterruptionTest-test-eks-1'
+$ export log_group_name='fis-log-group'
 
 
-export experiment__template_id=$(aws fis list-experiment-templates| jq -r  '.experimentTemplates[] | select(.tags.Name=="'"$cluster1_tag"'") | .id')
+experiment__template_id=$(aws fis list-experiment-templates| jq -r  '.experimentTemplates[] | select(.tags.Name=="'"$cluster1_tag"'") | .id' | awk 'NR==1 {print $1}') 
+aws fis start-experiment --experiment-template-id $experiment__template_id  --tags Name=$cluster1_tag & 
 
-aws fis start-experiment --experiment-template-id $target_experiment__template_id  --tags TargetCluster=$cluster1_tag & &&\
-aws fis list-experiments | jq -r  '.experiments[] | select(.tags.Name=="'"$cluster1_tag"'")'
+# 没必要
+experiment_id=$(aws fis list-experiments | jq -r  '.experiments[] | select(.tags.Name=="'"$cluster1_tag"'") | .id' | awk 'NR==1 {print $1}') 
 
-export experiment__template_id=$(aws fis list-experiment| jq -r  '.experiments[] | select(.tags.Name=="'"$cluster1_tag"'") | .id')
+aws logs filter-log-events --log-group-name $log_group_name --cli-input-json '{ "filterPattern": "{ $.id = \"'"$experiment_id"'\" }" }'
+
 
 
 export Target_Experiment_ID=$(aws fis list-experiment-templates| jq -r  '.experimentTemplates[] | select(.tags.Name=="'"$cluster2_tag"'") | .id')
 aws fis start-experiment --experiment-template-id $Target_Experiment_ID  --tags tag:TargetCluster=$cluster1_tag &
 aws fis list-experiments | jq -r  '.experiments[] | select(.tags.Name=="'"$cluster2_tag"'")'
+
+
+
+aws logs   filter-log-events --log-group-name 'fis-log-group'  --filter-pattern '{$.id = "EXPWCqm2EQJxd3YBSz"}'
+
+
+VENT_TYPE="UpdateTrail"
+aws logs filter-log-events --log-group-name <LOG_GROUP_NAME> --cli-input-json '{ "filterPattern": "{ $.eventType = \"'"$EVENT_TYPE"'\" }" }'
